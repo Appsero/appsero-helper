71d889d3060689a35a13051ecd3156739bc9588b
Sourov Roy <sourovfci@gmail.com>
2019-01-01 17:14:59 +0600

    Disable EDD license on cancel order

--- includes/SendRequests.php
+++ includes/SendRequests.php
@@ -1,6 +1,7 @@
 <?php
 namespace Appsero\Helper;
 
+use EDD_SL_License;
 use EDD_SL_Download;
 
 /**
@@ -12,15 +13,17 @@ class SendRequests {
     use Traits\Hooker;
 
     public function __construct() {
-        $this->action( 'edd_complete_download_purchase', 'add_license', 20, 5 );
+        $this->action( 'edd_complete_download_purchase', 'edd_add_license', 20, 5 );
 
-        // $this->action( 'rest_api_init', 'init_api' );
+        $this->action( 'edd_sl_post_revoke_license', 'edd_cancel_order', 10, 2 );
+
+        $this->action( 'edd_sl_post_delete_license', 'edd_cancel_order', 10, 2 );
     }
 
     /**
      * Send request to add license
      */
-    public function add_license( $download_id = 0, $payment_id = 0, $type = 'default', $cart_item = [], $cart_index = 0 ) {
+    public function edd_add_license( $download_id = 0, $payment_id = 0, $type = 'default', $cart_item = [], $cart_index = 0 ) {
         // Bail if this cart item is for a renewal
         if( ! empty( $cart_item['item_number']['options']['is_renewal'] ) ) {
             return;
@@ -63,6 +66,24 @@ class SendRequests {
             'source_identifier' => $license->id,
             'variation_source'  => (int) $license->price_id ?: '',
             'license_source'    => 'EDD',
+        ];
+
+        appsero_helper_remote_post( $route, $body );
+    }
+
+    /**
+     * Hook trigger after cancel order
+     * It will deactive a license on appsero server
+     */
+    public function edd_cancel_order( $license_id, $payment_id ) {
+        $license = new EDD_SL_License( $license_id );
+
+        $route = 'public/' . $license->download_id . '/cancel-order';
+
+        $body = [
+            'key'            => $license->key,
+            'license_source' => 'EDD',
+            'order_id'       => $payment_id,
         ];
 
         appsero_helper_remote_post( $route, $body );
