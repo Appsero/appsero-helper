b771d0160ecd767cc0eba9c582ae9fa7bccb4904
Sourov Roy <sourovfci@gmail.com>
2019-01-09 13:30:40 +0600

    Woo status change hooks for license added

--- includes/SendRequests.php
+++ includes/SendRequests.php
@@ -22,7 +22,9 @@ class SendRequests {
         $this->action( 'edd_sl_post_delete_license', 'edd_cancel_order', 10, 2 );
 
         // Woo Hooks
-        $this->action( 'woocommerce_order_status_completed', 'woo_add_license', 20, 1 );
+        $this->action( 'woocommerce_order_status_completed', 'woo_add_license', 20, 2 );
+
+        $this->action( 'woocommerce_order_status_changed', 'woo_status_changed', 20, 4 );
 
     }
 
@@ -98,14 +100,17 @@ class SendRequests {
     /**
      * Woocommerce send request to AppSero
      */
-    public function woo_add_license( $order_id ) {
+    public function woo_add_license( $order_id, $order ) {
+
+        // if WooCommerce Software Addon Exists
+        if ( class_exists( 'WC_Software' ) ) {
+            return $this->woo_sa_add_license( $order_id, $order );
+        }
 
         // if WooCommerce API Manager Exists
         if ( class_exists( 'WooCommerce_API_Manager' ) ) {
-            $this->woo_api_add_license( $order_id );
+            return $this->woo_api_add_license( $order_id, $order );
         }
-
-
     }
 
     /**
@@ -140,9 +145,9 @@ class SendRequests {
     /**
      * Send add license request to AppSero server
      */
-    private function woo_api_add_license( $order_id ) {
+    private function woo_api_add_license( $order_id, $order ) {
         if ( ! WC_AM_SUBSCRIPTION()->is_subscription_renewal_order( $order_id ) ) {
-            $order = wc_get_order( $order_id );
+            // $order = wc_get_order( $order_id );
             $api_keys_exist = WC_AM_HELPERS()->order_api_keys_exist( $order_id );
 
             $order_items = $order->get_items();
@@ -160,12 +165,134 @@ class SendRequests {
                             if ( ! empty( $license_key ) ) {
                                 $this->send_woo_api_add_license_request( $order->get_user_id(), $license_key, $product_id );
                             }
-                        }
+                        } // End for
                     }
 
-                }
+                } // End foreach
+            }
+        } // End if
+    }
+
+    /**
+     * Woocommerce status change
+     */
+    public function woo_status_changed( $order_id, $status_from, $status_to, $order ) {
+        $haystack = [ 'on-hold', 'cancelled', 'refunded', 'failed' ];
+
+        if ( 'completed' == $status_from && in_array( $status_to, $haystack ) ) {
+            // if WooCommerce Software Addon Exists
+            if ( class_exists( 'WC_Software' ) ) {
+                return $this->woo_sa_cancel_order( $order_id, $order );
+            }
+
+            // if WooCommerce API Manager Exists
+            if ( class_exists( 'WooCommerce_API_Manager' ) ) {
+                return $this->woo_api_cancel_order( $order_id, $order );
             }
         }
     }
+
+    /**
+     * Send request to appsero server to add license
+     * For Woo SA
+     */
+    private function woo_sa_add_license( $order_id, $order ) {
+        global $wpdb;
+
+        $order_items = $order->get_items();
+        if ( sizeof( $order_items ) == 0 ) {
+            return;
+        }
+
+        foreach ( $order_items as $order_item ) {
+            $product_id  = $order_item->get_product_id();
+            $software_id = get_post_meta( $product_id, '_software_product_id', true);
+
+            $query = "SELECT * FROM {$wpdb->wc_software_licenses} WHERE `order_id` = {$order_id} AND `software_product_id` = '{$software_id}' ";
+            $license = $wpdb->get_row( $query, ARRAY_A );
+
+            if ( ! empty( $license ) ) {
+                $this->send_woo_sa_add_license_request( $product_id, $license );
+            }
+        }
+    }
+
+    /**
+     * Woo SA send add license request
+     */
+    private function send_woo_sa_add_license_request( $product_id, $license ) {
+        $route = 'public/' . $product_id . '/add-license';
+
+        $body = [
+            'key'              => $license['license_key'],
+            'status'           => 1,
+            'activation_limit' => $license['activations_limit'] ?: '',
+            'active_sites'     => 0,
+            'expire_date'      => '',
+            'variation_source' => '',
+            'license_source'   => 'Woo SA',
+        ];
+
+        appsero_helper_remote_post( $route, $body );
+    }
+
+    /**
+     * Woo SA cancel order
+     * It will deactive a license on appsero server
+     */
+    private function woo_sa_cancel_order( $order_id, $order ) {
+        global $wpdb;
+
+        $order_items = $order->get_items();
+        if ( sizeof( $order_items ) == 0 ) {
+            return;
+        }
+
+        foreach ( $order_items as $order_item ) {
+            $product_id  = $order_item->get_product_id();
+            $software_id = get_post_meta( $product_id, '_software_product_id', true);
+
+            $query   = "SELECT * FROM {$wpdb->wc_software_licenses} WHERE `order_id` = {$order_id} AND `software_product_id` = '{$software_id}' ";
+            $license = $wpdb->get_row( $query, ARRAY_A );
+
+            if ( ! empty( $license ) ) {
+                $route = 'public/' . $product_id . '/cancel-order';
+
+                $body = [
+                    'key'            => $license['license_key'],
+                    'license_source' => 'Woo SA',
+                    'order_id'       => $order_id,
+                ];
+
+                appsero_helper_remote_post( $route, $body );
+            }
+        } // End foreach
+    }
+
+    /**
+     * Woo SA cancel order
+     * It will deactive a license on appsero server
+     */
+    private function woo_api_cancel_order( $order_id, $order ) {
+        $user_id     = $order->get_user_id();
+        $user_orders = WC_AM_HELPERS()->get_users_data( $user_id );
+
+        if ( ! empty( $user_orders ) ) {
+            foreach ( $user_orders as $order_key => $data ) {
+                if ( $data['order_id'] == $order_id ) {
+                    $route = 'public/' . $data['parent_product_id'] . '/cancel-order';
+
+                    $body = [
+                        'key'            => $data['api_key'],
+                        'license_source' => 'Woo API',
+                        'order_id'       => $order_id,
+                    ];
+
+                    appsero_helper_remote_post( $route, $body );
+                }
+            } // End foreach
+        } // End if
+    }
+
 
 }
