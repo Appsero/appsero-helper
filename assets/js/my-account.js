f76bb2b1d1a410a0ec48270130a267a1b40b6d7e
Sourov Roy <sourovfci@gmail.com>
2019-09-17 11:34:11 +0600

    Fix license arrow button not showing

--- assets/js/my-account.js
+++ assets/js/my-account.js
@@ -6,11 +6,11 @@ jQuery( document ).ready( function () {
 
         if ( showingInfo == 1 ) {
             parent.find( '.license-key-activations' ).hide();
-            jQuery( this ).children( 'i.fas' ).removeClass( 'fa-angle-up' ).addClass( 'fa-angle-down' );
+            jQuery( this ).removeClass( 'license-button-toggled' );
             parent.data( 'showing', 0 );
         } else {
             parent.find( '.license-key-activations' ).show();
-            jQuery( this ).children( 'i.fas' ).removeClass( 'fa-angle-down' ).addClass( 'fa-angle-up' );
+            jQuery( this ).addClass( 'license-button-toggled' );
             parent.data( 'showing', 1 );
         }
     } );
