457900cade4a61a621e50459b735177b9568e38a
Sourov Roy <sourovfci@gmail.com>
2020-04-05 09:09:28 +0600

    Bump the version and update change log

--- appsero-helper.php
+++ appsero-helper.php
@@ -5,7 +5,7 @@
  * Description: Helper plugin to connect WordPress store to Appsero
  * Author: Appsero
  * Author URI: https://appsero.com
- * Version: 1.1.6
+ * Version: 1.1.7
  * Text Domain: appsero-helper
  */
 
@@ -24,7 +24,7 @@ class Appsero_Helper {
      *
      * @var string
      */
-    public $version = '1.1.5';
+    public $version = '1.1.7';
 
     /**
      * The single instance of the class.
