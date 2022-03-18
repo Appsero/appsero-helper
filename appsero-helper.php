5f048e63dc65c4bf4588091c981feb18d66ecb02
Tareq Hasan <tareq1988@gmail.com>
2022-03-18 15:04:35 +0300

    Bumped version to 1.2.1

--- appsero-helper.php
+++ appsero-helper.php
@@ -5,7 +5,7 @@
  * Description: Helper plugin to connect WordPress store to Appsero
  * Author: Appsero
  * Author URI: https://appsero.com
- * Version: 1.2.0
+ * Version: 1.2.1
  * Text Domain: appsero-helper
  */
 
@@ -24,7 +24,7 @@ final class Appsero_Helper {
      *
      * @var string
      */
-    public $version = '1.2.0';
+    public $version = '1.2.1';
 
     /**
      * The single instance of the class.
